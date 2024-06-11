e27df4e9ea8b1050aeffc0f24f0a08e29735a2e1
Vishal <112974492+chewbaqa@users.noreply.github.com>
2024-06-11 13:23:23 +0530

    fix: L4 finished
    Writeup for Level 4 is now finished.

--- Intercepting Communication/l4.sh
+++ Intercepting Communication/l4.sh
@@ -3,3 +3,15 @@
 # In this challenge you will find and connect to a remote host.
 # The remote host is somewhere on the `10.0.0.0/16` subnetwork, listening on port `31337`.
 
+nmap -sX 10.0.0.0/16 -p 31337 -T5 --min-rate 2000 --max-retries 0
+
+# Nmap scan report for 10.0.120.150
+# Host is up (0.000043s latency).
+
+# PORT      STATE         SERVICE
+# 31337/tcp open|filtered Elite
+# MAC Address: A6:BC:A0:0F:EE:A1 (Unknown)
+
+# Nmap done: 65536 IP addresses (2 hosts up) scanned in 68.12 seconds
+
+nc 10.0.120.150 31337
